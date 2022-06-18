d6b531de28c9c7e360b5fb759389585c0a43d044
Pavel <105745109+UPashka@users.noreply.github.com>
2022-06-18 16:34:58 +0300

    Start hw6

---
+++ hw6/scripts.js
@@ -0,0 +1,248 @@
+// Exercise 1
+
+function powersOfTwo(n){
+    let arr = [],
+        num = 1;
+        
+        for (let i = 0; i <= n; i++) {
+          if (i == 0) { 
+              arr.push(1);
+          } else {
+           num *= 2;
+           arr.push(num);
+        }
+        }
+      return arr;
+  }
+  
+
+console.log(powersOfTwo(3));
+
+// Exercise 2
+
+// function powersOfTwo(n){
+//     let arr = [],
+//         num = 0;
+//         for (let i = 0; i <= n; i++) {
+//         num = 2**i;
+    
+//         arr.push(num);
+//     }
+//       return arr;
+//   }
+  
+
+// console.log(powersOfTwo(1));
+
+// Exercise 3
+
+// function oddCount(n){
+//   let arr = [];
+//       for (let i = 0; i < n; i++) {
+//         if (i % 2 == 1) arr.push(i);
+//       }
+//   return arr.length;
+// }
+
+// console.log(oddCount(7));
+
+
+// Exercise 4
+
+// function squareSum(numbers){
+//   let sum = 0;
+  
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i]**2;
+//   }
+//   return sum;
+// }
+
+// console.log(squareSum([1, 2, 2]));
+
+
+// Exercise 5
+
+// for input:                5, true, false
+// expected result would be: [true, false, true, false, true]
+
+// const arr = function(n, firstValue, secondValue) {
+//       let arr = [];
+//       for (let i = 0; i < n; i++) {
+//         if (i % 2 == 0) arr.push(firstValue);
+//         else arr.push(secondValue);
+//       }
+//       return arr;
+// }
+
+// console.log(arr(7, true, false))
+
+
+// Exercise 6
+
+// each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+// each_cons([1,2,3,4], 3)
+//   #=> [[1,2,3],[2,3,4]]
+
+
+// const each_cons2 = function(arr, n) {
+//   let arr1 = [];
+// if (n%2 == 0) {
+// for (let i = 0; i < arr.length-1; i++) {
+//    arr1.push(arr.slice(i,i+n));
+// }
+// } else if (n == 1) {
+//   for (let i = 0; i < arr.length; i++) {
+//     arr1.push(arr.slice(i, i+n));
+//  }
+// }
+
+// else if (n%2 == 1) {
+//   for (let i = 0; i < arr.length-2; i++) {
+//     arr1.push(arr.slice(i,i+n));
+//  }
+// } else {
+//   arr1 = [];
+// }
+//   return arr1;
+// }
+
+// console.log(each_cons2([1,2,3,4], 3));
+
+
+// Exercise 7
+
+// 16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+
+// const sumNum = function (num) {
+//     let sum = 0;
+//       num = String(num);
+
+//       for (let i = 0; i < num.length; i++) {
+//         sum += +num[i];
+//       }
+
+//       if (String(sum).length > 1) {
+//         sumNum(sum);
+//         return;
+//       } else {
+//       console.log(sum);
+//       }
+
+// }
+// sumNum(0);
+// console.log(sumNum(165));
+
+
+// Exercise 8
+
+// function lovefunc(flower1, flower2){
+//   let sum = flower1 + flower2;
+//    if (sum % 2 == 1) return true;
+//     else return false;
+// }
+
+// console.log(lovefunc(0, 1));
+
+// Exercise 9
+
+// Example (if i is 25):
+
+// Multiples of 25 up to 100 --> [25, 50, 75, 100]
+
+// Digit sum of each multiple --> [7, 5, 12, 1]
+
+// Sum of each new digit sum --> 25
+
+
+
+const numb = function (num) {
+let arrNumb = [];
+
+for (let i = 1; i <= 100; i++) {
+    if (i % num == 0) arrNumb.push(i)
+}
+
+console.log(arrNumb);
+
+let arr2 = [];
+arrNumb.forEach(function(item) {
+  item = String(item).split('');
+  let sum = 0;
+    item.map(function(item) {
+      sum += +item;
+    })
+    arr2.push(sum); 
+})
+console.log(arr2);
+
+let sum = 0;
+let arr3 = [];
+arr2.map(function(item){
+  sum += +item;
+})
+arr3.push(sum);
+
+console.log(arr3.join());
+
+}
+
+numb(10);
+
+
+// 4. Напишите функцию findLongestWord(str), которая принимает строку в качестве параметра и находит самое длинное слово в строке.
+
+const  findLongestWord = function(str) {
+    let words = str.split(' ');
+    let maxLength = words[0].length;
+    let wordLong = '';
+
+    // console.log(words);
+
+    words.forEach(function(item) {
+      if (maxLength <= item.length) {
+        maxLength = item.length;
+      wordLong = item;
+      }
+    });
+  
+    let arr2 = [];
+    words.filter(function(item) {
+      if (item.length >= 13) {
+        arr2.push(item);
+      }
+      return arr2;
+      
+    });
+    
+    console.log(arr2.join(' '));
+  }
+  
+findLongestWord('Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую  коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.');
+
+// 1. Напишите функцию, которая переворачивает число, например: 5675 => 5765.
+
+const revNumb = function(numb) {
+    numb = String(numb).split('').reverse().join('');
+    return  console.log(numb);
+}
+
+revNumb(5675);
+
+
+// 2. Напишите функцию, которая проверяет, является ли переданная строка палиндромом.
+
+const matchStr = function(str) {
+    if (str.toLowerCase() == str.toLowerCase().split('').reverse().join('')) return true;
+     else return false;
+
+}
+
+console.log(matchStr ('aMna'));
+
+// 3. Напишите функцию alphabetOrder(str), которая возвращает переданную строку с буквами в алфавитном порядке. Пример строки: 'alphabetical'. Ожидаемый результат: 'aaabcehillpt'. Предположим, что символы пунктуации и цифры не включены в переданную строку.
+
